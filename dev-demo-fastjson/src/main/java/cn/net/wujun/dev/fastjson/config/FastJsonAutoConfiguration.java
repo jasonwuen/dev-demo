674f907a4692295f22fd5b9911cdc7dd262befd9
wujun <wujun@miao.cn>
2017-05-30 14:16:04 +0800

    fix:update assembly-descriptor

---
+++ dev-demo-fastjson/src/main/java/cn/net/wujun/dev/fastjson/config/FastJsonAutoConfiguration.java
@@ -0,0 +1,54 @@
+package cn.net.wujun.dev.fastjson.config;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.PropertyNamingStrategy;
+import com.alibaba.fastjson.serializer.SerializeConfig;
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ValueFilter;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * FastJsonHttpMessageConvertersConfiguration.
+ */
+@Configuration
+@ConditionalOnProperty("http.converters.fastjson")
+public class FastJsonAutoConfiguration {
+
+    protected final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Bean
+    public FastJsonHttpMessageConverter fastJsonHttpMessageConverter() {
+        logger.info("fastJsonHttpMessageConverter init");
+        SerializeConfig.getGlobalInstance().propertyNamingStrategy = PropertyNamingStrategy
+                .SnakeCase;
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteNullStringAsEmpty
+        );
+        ValueFilter valueFilter = new ValueFilter() {
+            //o 是class
+            //s 是key值
+            //o1 是value值
+            public Object process(Object o, String s, Object o1) {
+                if (null == o1) {
+                    o1 = "";
+                }
+                return o1;
+            }
+        };
+        fastJsonConfig.setSerializeFilters(valueFilter);
+        converter.setFastJsonConfig(fastJsonConfig);
+        return converter;
+    }
+}
--- dev-demo-rest/src/main/java/cn/net/wujun/dev/demo/controller/HelloController.java
+++ dev-demo-rest/src/main/java/cn/net/wujun/dev/demo/controller/HelloController.java
@@ -1,12 +1,16 @@
 package cn.net.wujun.dev.demo.controller;
 
 import cn.net.wujun.dev.demo.service.HelloService;
+import com.google.common.collect.ImmutableMap;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * Created by Jason Wu on 2017/05/25.
@@ -22,7 +26,17 @@ public class HelloController {
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name) {
         logger.debug("--controller->>>>>>>>hello param:{}", name);
+        logger.info("--controller->>>>>>>>hello param:{}", name);
+        logger.warn("--controller->>>>>>>>hello param:{}", name);
+        logger.error("--controller->>>>>>>>hello param:{}", name);
         String result = helloService.hello(name);
         return result;
+    }
+
+    @GetMapping("/json")
+    public Map<Object, Object> json() {
+        ImmutableMap<Object, Object> build = ImmutableMap.builder().put("name", "zhangsam").put
+                ("age", 20).put("gmCreater", DateTime.now().getMillis()).build();
+        return build;
     }
 }
